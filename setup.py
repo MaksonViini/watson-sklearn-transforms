b372de5e0535b5350acb6e2fa681f302db1f02c3
MaksonViini <maksonvinicio7@gmail.com>
2021-11-14 11:55:44 -0300

    Adicionando requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
+import os
 
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+
+install_requires = []  # Here we'll get: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name='my_custom_sklearn_transforms',
@@ -15,5 +23,6 @@ setup(
     packages=[
         'my_custom_sklearn_transforms'
     ],
-    zip_safe=False
+    zip_safe=False,
+    install_requires=install_requires
 )
